a4698276a335b32505dc3a434f5bc11346427a98
jkill <j_kill@qq.com>
2021-06-07 18:10:33 +0800

    112. 路径总和-DFS解法

--- 2021-04-25/HasPathSum.java
+++ 2021-04-25/HasPathSum.java
@@ -1,52 +1,51 @@
-package haiwaitu.t20210425;
+package haiwaitu.t20210606;
 
+import haiwaitu.TreeNode;
+
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * @Author huangjunqiao
- * @Date 2021/04/25 11:44
+ * @Date 2021/06/07 17:21
  * @Description 112. 路径总和
  */
 public class HasPathSum {
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-        }
-    }
-    public boolean hasPathSum(TreeNode root, int targetSum) {
+    public boolean hasPathSum0(TreeNode root, int targetSum) {
+        // DFS解法，时间每个节点访问一次O(N)，空间为树高，平均情况下O(logN)，最坏情况退化成链表O(N)
         if (root == null) {
             return false;
         }
-        // 广度优先搜索BFS
-        Queue<TreeNode> nodeQueue = new LinkedList<>();
-        Queue<Integer> valQueue = new LinkedList<>();
-        nodeQueue.offer(root);
+        targetSum -= root.val;
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        // BFS解法，时间每个节点进出队列一次O(N)，空间为队列大小，最坏情况完美二叉树底层有N/2个节点，即O(N)
+        if (root == null) {
+            return false;
+        }
+        Deque<TreeNode> q = new LinkedList<>();
+        Deque<Integer> valQueue = new LinkedList<>();
+        q.offer(root);
         valQueue.offer(root.val);
-        while (!nodeQueue.isEmpty()) {
-            TreeNode currNode = nodeQueue.poll();
-            int currVal = valQueue.poll();
-            if (currNode.left == null && currNode. right == null) {
-                if (currVal == targetSum) {
-                    return true;
-                } else {
-                    continue;
-                }
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            int currSum = valQueue.poll();
+            // 发现叶子节点，判断当前路径和是否等于目标和，否则继续遍历
+            if (node.left == null && node.right == null && currSum == targetSum) {
+                return true;
             }
-            if (currNode.left != null) {
-                nodeQueue.offer(currNode.left);
-                valQueue.offer(currVal + currNode.left.val);
+            if (node.left != null) {
+                q.offer(node.left);
+                valQueue.offer(currSum + node.left.val);
             }
-            if (currNode.right != null) {
-                nodeQueue.offer(currNode.right);
-                valQueue.offer(currVal + currNode.right.val);
+            if (node.right != null) {
+                q.offer(node.right);
+                valQueue.offer(currSum + node.right.val);
             }
         }
         return false;
