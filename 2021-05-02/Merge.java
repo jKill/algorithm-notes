20fbf66860b380c80aa779fc1ae62f1ab0268b72
jkill <j_kill@qq.com>
2021-06-18 13:36:48 +0800

    补充时空复杂度：88. 合并两个有序数组

--- 2021-05-02/Merge.java
+++ 2021-05-02/Merge.java
@@ -8,11 +8,10 @@ package haiwaitu.t20210502;
 public class Merge {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        // 逆向双指针合并，从后往前放置元素，这样不需要移动其他元素
-        int[] merge = new int[m + n];
+        // 逆向双指针合并，从后往前放置元素，这样不需要移动其他元素，时间O(m+n)，空间O(1)
         int idx = m + n - 1;
-        m-=1;
-        n-=1;
+        m--;
+        n--;
         // 当nums1和nums2数组都还有元素时，从数组末尾比较大小并放置大的元素
         while (m >= 0 && n >= 0) {
             if (nums1[m] < nums2[n]) {
