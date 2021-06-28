0e39ed4aa552321064e13575293b6251d3b88bcf
jkill <j_kill@qq.com>
2021-06-28 10:34:38 +0800

    更简洁的快慢指针：19. 删除链表的倒数第 N 个结点

--- 2021-04-24/RemoveNthFromEnd.java
+++ 2021-04-24/RemoveNthFromEnd.java
@@ -1,49 +1,37 @@
 package haiwaitu.t20210424;
 
+import haiwaitu.ListNode;
+
 /**
  * @Author huangjunqiao
  * @Date 2021/04/24 17:05
  * @Description 19. 删除链表的倒数第 N 个结点
  */
 public class RemoveNthFromEnd {
-    class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) {
-            val = x;
-            next = null;
-        }
-    }
-
     /**
      * 删除倒数第N个节点，两趟扫描可以很容易的实现，但是属于暴力解法
      * 要实现一趟扫描，可以通过栈"后进先出"的特性实现，但是需要O(n)的空间
-     * 更优雅的解法是双指针，只需要O(1)的空间。其中一个指针先走n步，
+     * 更优雅的解法是快慢指针，只需要O(1)的空间。其中一个指针先走n步，
      * 因为是删除操作，需要记录待删除节点的前驱节点。
      * @param head
      * @param n
      * @return
      */
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode toDelPrev = new ListNode(0);
-        toDelPrev.next = head;
-        ListNode node = head;
-        for (int i = 0; i < n; i++) {
-            node = node.next;
+        // 快慢指针，时间O(N)，空间O(1)
+        ListNode dummy = new ListNode(-1, head);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
         }
-        // node为空说明要删除的是头节点
-        if (node == null) {
-            ListNode headNext = head.next;
-            head.next = null;
-            return headNext;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
         }
-        while (node != null) {
-            toDelPrev = toDelPrev.next;
-            node = node.next;
-        }
-        // 找到待删除节点，进行删除
-//        ListNode toDel = toDelPrev.next;
-        toDelPrev.next = toDelPrev.next.next;
-        return head;
+        // do delete
+        slow.next = slow.next.next;
+        return dummy.next;
     }
 }
