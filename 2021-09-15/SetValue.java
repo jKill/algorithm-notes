08084fe6cf28ee28517ec0d2abfa1688999ebb5c
jkill <j_kill@qq.com>
2021-09-17 21:28:13 +0800

    更正位运算-设置值

--- 2021-09-15/SetValue.java
+++ 2021-09-15/SetValue.java
@@ -14,8 +14,7 @@ public class SetValue {
      * @return
      */
     public int setK1(int num, int k) {
-        num |= 1 << (k - 1);
-        return num < 0 ? Integer.MAX_VALUE : num;
+        return num | 1 << k;
     }
 
     /**
@@ -26,16 +25,8 @@ public class SetValue {
      * @return
      */
     public int setK0(int num, int k) {
-        // 1、考虑到0与任何数异或，都能保持原数，使用异或
-        // 2、又考虑1与任何数异或，都能将数取反。可以分两种情况讨论。
-        // 一、第k位如果为0，则不需要取反(与0异或)；二、如果第k位为1，则取反(与1异或)
-        int kthBit = num >> (k - 1);
-        if ((kthBit & 1) == 0) {
-            num ^= 0;// 不需要取反，保持原数
-        } else {
-            num ^= 1 << (k - 1);// 第k位为1，需要取反
-        }
-        return num;
+
+        return num & ~(1 << k);
     }
 
     /**
@@ -46,7 +37,7 @@ public class SetValue {
      * @return
      */
     public int reverseKth(int num, int k) {
-        return num ^ 1 << (k - 1);
+        return num ^ 1 << k;
     }
 
 }
