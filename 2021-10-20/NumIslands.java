fa03d9458cbc2d2ff1374f3803d4264908761a40
jkill <j_kill@qq.com>
2021-10-24 02:41:56 +0800

    补充：200. 岛屿数量-并查集解法

--- 2021-10-20/NumIslands.java
+++ 2021-10-20/NumIslands.java
@@ -72,4 +72,75 @@ public class NumIslands {
          return islandNum;
      }
 
+
+
+    class UnionFind {
+        public int[] parent;
+        public int[] rank;
+        public int count;// 独立的岛屿数量
+
+        public UnionFind(char[][] grid) {
+            // init
+            int m = grid.length, n = grid[0].length;
+            count = 0;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {// 所有'1'作为根节点
+                        parent[i * n + j] = i * n + j;
+                        count++;// 先统计全部岛屿，之后合并再做减法
+                    }
+                }
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] == x) {
+                return x;
+            }
+            return find(parent[x]);
+        }
+
+        public void union(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx;
+                    rank[rootx]++;
+                }
+                count--;
+            }
+        }
+    }
+    public int numIslands1(char[][] grid)  {
+        // 并查集解法，时间：O(MNα(MN))，空间：O(MN)
+        int m = grid.length, n = grid[0].length;
+        UnionFind uf = new UnionFind(grid);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    grid[i][j] = '0';// 及时清零防止重复统计
+                    if (i - 1 >= 0 && grid[i - 1][j] == '1') {
+                        uf.union(i * n + j, (i - 1) * n + j);
+                    }
+                    if (i + 1 < m && grid[i + 1][j] == '1') {
+                        uf.union(i * n + j, (i + 1) * n + j);
+                    }
+                    if (j - 1 >= 0 && grid[i][j - 1] == '1') {
+                        uf.union(i * n + j, i * n + j - 1);
+                    }
+                    if (j + 1 < n && grid[i][j + 1] == '1') {
+                        uf.union(i * n + j, i * n + j + 1);
+                    }
+                }
+            }
+        }
+        return uf.count;
+    }
 }
