b4919901cb2873ee75941f1c077cdcb705038ca7
jkill <j_kill@qq.com>
2022-01-14 18:27:08 +0800

    300. 补充二分解法

--- 2021-04-26/LengthOfLIS.java
+++ 2021-04-26/LengthOfLIS.java
@@ -13,27 +13,50 @@ public class LengthOfLIS {
      * @param nums
      * @return
      */
-    public static int lengthOfLIS(int[] nums) {
-        // 动态规划解法
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int[] dp = new int[nums.length];
-        // 至少长度为1
-        dp[0] = 1;
-        int result = 1;
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                // dp[i]代表以nums[i]为结尾的子序列，这里才可以这样比较
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+     public int lengthOfLIS(int[] nums) {
+         // dp，时间：O(n^2)，空间：O(n)
+         int n = nums.length;
+         int max = 0;
+         int[] dp = new int[n];
+         for (int i = 0; i < n; i++) {
+             dp[i] = 1;
+             for (int j = 0; j < i; j++) {
+                 if (nums[i] > nums[j]) {
+                     dp[i] = Math.max(dp[i], dp[j] + 1);
+                 }
+             }
+             max = Math.max(max, dp[i]);
+         }
+         return max;
+     }
+
+    public int lengthOfLIS0(int[] nums) {
+        // 二分+dp，时间：O(nlogn)，空间：O(n)
+        int n = nums.length;
+        int[] d = new int[n + 1];// dp[i]为长度为i的子序列中，末尾最小数字
+        int len = 1;
+        d[1] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (d[len] < nums[i]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len;
+                while (l < r) {
+                    int mid = (l + r) / 2;
+                    if (d[mid] < nums[i]) {
+                        l = mid + 1;
+                    } else {
+                        r = mid;
+                    }
+                }
+                if (d[1] > nums[i]) {
+                    d[1] = nums[i];
+                } else {
+                    d[r] = nums[i];
                 }
             }
-            // 更新最大长度
-            result = Math.max(dp[i], result);
         }
-        return result;
+        return len;
     }
 
 }
