86a20b792613e7f9f888cd7283920b736ef2e818
jkill <j_kill@qq.com>
2022-02-21 17:23:54 +0800

    114. 二叉树展开为链表：更简洁的morris解法(递归&迭代版)

--- 2021-05-31/Flatten.java
+++ 2021-05-31/Flatten.java
@@ -8,49 +8,42 @@ import haiwaitu.TreeNode;
  * @Description 114. 二叉树展开为链表
  */
 public class Flatten {
-     public void flatten(TreeNode root) {
-         // 非主流解法，原地递归，时间O(n)，空间O(n)
-         flattenCore(root);
-         return;
-     }
-     public TreeNode flattenCore(TreeNode node) {
-         // 先处理左子节点
-         if (node.left != null) {
-             // 缓存右子节点，左子节点移到右边并递归处理，处理后最右边的节点rightPrev拼接到原来的右子节点前面
-             TreeNode originRight = node.right;
-             node.right = node.left;
-             node.left = null;
-             TreeNode rightPrev = flattenCore(node.right);
-             // 最右边的节点rightPrev接上原来的右节点
-             rightPrev.right = originRight;
-             // 节点指向处理完的最后一个节点
-             node = rightPrev;
-         }
-         // 处理右子节点
-         if (node.right != null) {
-             node = flattenCore(node.right);
-         }
-         return node;
-     }
+    public void flatten(TreeNode root) {
+        // 递归版寻找前驱节点，时间：O(n)，空间：O(n)
+        if (root == null) {
+            return;
+        }
+        dfs(root);
+    }
+    public TreeNode dfs(TreeNode node) {// 返回展开后的最后一个节点
+        if (node.left != null) {
+            TreeNode origRight = node.right;
+            node.right = node.left;
+            node.left = null;
+            TreeNode flatten = dfs(node.right);
+            flatten.right = origRight;
+            node = flatten;
+        }
+        if (node.right != null) {
+            node = dfs(node.right);
+        }
+        return node;
+    }
 
     public void flatten0(TreeNode root) {
-        // 原地前驱指针迭代，时间O(n)，空间O(1)
-        TreeNode curr = root;
-        TreeNode prev = null;
-        while (curr != null) {
-            if (curr.left != null) {
-                prev = curr.left;
-                // 找到左子节点的最右子节点prev
-                while (prev.right != null) {
-                    prev = prev.right;
+        // 迭代版寻找前驱节点，时间：O(n)，空间：O(1)
+        TreeNode pre = null;
+        while (root != null) {
+            TreeNode left = root.left;
+            if (left != null) {
+                while (left.right != null) {
+                    left = left.right;
                 }
-                // 把当前右子节点拼接到prev后面
-                prev.right = curr.right;
-                // 当前左子节点移到右子节点的位置
-                curr.right = curr.left;
-                curr.left = null;
+                left.right = root.right;
+                root.right = root.left;
+                root.left = null;
             }
-            curr = curr.right;
+            root = root.right;
         }
     }
 }
