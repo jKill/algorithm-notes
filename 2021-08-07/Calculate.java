606d336e4406e281de0564f6aab78b5340b0aaef
jkill <j_kill@qq.com>
2022-08-02 16:13:00 +0800

    227. 基本计算器 II 补充O(1)空间解法

--- 2021-08-07/Calculate.java
+++ 2021-08-07/Calculate.java
@@ -41,4 +41,48 @@ public class Calculate {
         }
         return ans;
     }
+
+    int i = 0;
+    public int calculate0(String s) {
+        // 变量代替栈，时间：O(N)，空间：O(1)
+        int isPositive = 1;
+        int n = s.length();
+        int preNum = 0;
+        int res = 0;
+        while (i < n) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                i++;
+            } else if (c == '+') {
+                res += preNum * isPositive;
+                isPositive = 1;
+                i++;
+            } else if (c == '-') {
+                res += preNum * isPositive;
+                isPositive = -1;
+                i++;
+            } else if (c == '*') {
+                int num = getNextNum(s, n);
+                preNum *= num;
+            } else if (c == '/') {
+                int num = getNextNum(s, n);
+                preNum /= num;
+            } else {
+                preNum = getNextNum(s, n);
+            }
+        }
+        res += preNum * isPositive;
+        return res;
+    }
+    public int getNextNum(String s, int n) {
+        int num = 0;
+        while (i < n && !Character.isDigit(s.charAt(i))) {
+            i++;
+            continue;
+        }
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            num = num * 10 + s.charAt(i++) - '0';
+        }
+        return num;
+    }
 }
